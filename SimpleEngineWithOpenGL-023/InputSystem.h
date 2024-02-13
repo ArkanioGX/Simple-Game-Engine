9cf6e863bfdfe25f5ce526bd60819824a3048639
ArkanioGX <clarencemollet1@gmail.com>
2024-02-13 12:34:11 +0100

    Mouse Input

--- SimpleEngineWithOpenGL-023/InputSystem.h
+++ SimpleEngineWithOpenGL-023/InputSystem.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "KeyboardState.h"
+#include "MouseState.h"
+#include <SDL_events.h>
 
 enum class ButtonState {
 	None,
@@ -11,6 +13,7 @@ enum class ButtonState {
 struct InputState
 {
 	KeyboardState keyboard;
+	MouseState mouse;
 };
 
 class InputSystem {
@@ -20,12 +23,19 @@ public:
 	bool initialize();
 	void close();
 
+	bool processEvent(SDL_Event& event);
+
 	const InputState getInputState() const { return inputState; }
 
 	void preUpdate();
 	void update();
 
+	bool getIsCursorDisplayed() const { return isCursorDisplayed; }
+	void setMouseCursor(bool isCursorDisplayedP);
+	void setMouseRelativeMode(bool isMouseRelativeOnP);
+
 private:
 	InputState inputState;
+	bool isCursorDisplayed;
 };
 
